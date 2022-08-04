5cea7954b765b57bb4cfbc34a618468e9b0f95e6
JosephPark <joeepark@gmail.com>
2022-08-04 13:16:38 -0700

    Added another project called feedback-ui and currently working on it to build it out

---
+++ feedback-ui/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
