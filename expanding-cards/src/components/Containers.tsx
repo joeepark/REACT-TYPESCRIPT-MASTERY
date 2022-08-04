f0291803f6d57a50aad13b52e7adf488885f0741
JosephPark <joeepark@gmail.com>
2022-08-04 11:11:14 -0700

    90% completed. I need to add one more JS functionality to finish

--- expanding-cards/src/components/Containers.tsx
+++ expanding-cards/src/components/Containers.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import image1 from '../assets/1.jpg';
 import image2 from '../assets/2.jpg';
 import image3 from '../assets/3.jpg';
@@ -5,28 +6,50 @@ import image4 from '../assets/4.jpg';
 import image5 from '../assets/5.jpg';
 
 export default function Container() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick: any = (e) => {
+    e.preventDefault();
+    setIsActive((current) => !current);
+  };
+
   return (
     <>
       <div
-        style={{ backgroundImage: `url(${image1})`}}
-        className="containers active"
-      ></div>
+        style={{ backgroundImage: `url(${image1})` }}
+        className={isActive ? 'cards active' : 'cards'}
+        onClick={handleClick}
+      >
+        <h3>Travel the world!</h3>
+      </div>
       <div
         style={{ backgroundImage: `url(${image2})` }}
-        className="containers"
-      ></div>
+        className={isActive ? 'cards active' : 'cards'}
+        onClick={handleClick}
+      >
+        <h3>Travel the world!</h3>
+      </div>
       <div
         style={{ backgroundImage: `url(${image3})` }}
-        className="containers"
-      ></div>
+        className={isActive ? 'cards active' : 'cards'}
+        onClick={handleClick}
+      >
+        <h3>Travel the world!</h3>
+      </div>
       <div
         style={{ backgroundImage: `url(${image4})` }}
-        className="containers"
-      ></div>
+        className={isActive ? 'cards active' : 'cards'}
+        onClick={handleClick}
+      >
+        <h3>Travel the world!</h3>
+      </div>
       <div
         style={{ backgroundImage: `url(${image5})` }}
-        className="containers"
-      ></div>
+        className={isActive ? 'cards active' : 'cards'}
+        onClick={handleClick}
+      >
+        <h3>Travel the world!</h3>
+      </div>
     </>
   );
 }
